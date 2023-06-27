f461e185aa903587882aae469ec7be308c62b01b
junaid-o <junaidmohd795@gmail.com>
2023-06-27 12:09:38 +0530

    update

---
+++ ThyroidPrediction/secrets/secret.py
@@ -0,0 +1,8 @@
+AWS_S3_ACCESS_KEY_ID = "*******"
+AWS_S3_SECRET_ACCESS_KEY = "*******"
+AWS_REGION = "ap-south-1"
+
+
+# Define the bucket and folder name
+AWS_BUCKET_NAME = '*******'
+AWS_FOLDER_NAME = '*******'
